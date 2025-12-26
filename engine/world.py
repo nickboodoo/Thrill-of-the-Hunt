3401faedad51291c13a7a1d2d2d0167906f4bade
nickboodoo <nickboodoo96@outlook.com>
2025-12-26 10:00:00 -0500

    added map class

---
+++ engine/world.py
@@ -0,0 +1,10 @@
+# map class will own the world map
+
+class World():
+    def __init__(self):
+        self.map_exists = True
+    
+    def generate_map(size):
+        world = [] * size
+        return f"Map has been generated of size {size}"
+    
